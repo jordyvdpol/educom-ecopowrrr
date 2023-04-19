3c6d24d41c07f37bc64345d1d4f3aa9f9d0808f5
jordyvdpol <jordyvdpol@gmail.com>
2023-04-19 14:43:02 +0200

    get by id

--- express-api/index.js
+++ express-api/index.js
@@ -12,18 +12,23 @@ app.listen(3000, () => {
 
 // --------------------------------------------------- GET ---------------------------------------------------------
 
-
-
 app.get('/', (request, response) => {
     /// First call the list function/promise from the mongo class
-    mongo.fetch()
+    mongo.fetch('test')
          .then(result => response.send(result))
          .catch (err => response.send(err))
+})
+
+app.get('/:id', (request, response) => {
+    mongo.fetch('test', request.params.id)
+        .then (result => response.send(result))
+        .catch (err => response.send(err))
 })
 
 
 const courses = 
     {
+        // '_id': '1',
         "name": "Express Server API",
         "level": "intermediate",
         "technology": ["JavaScript", "NodeJS", "Express", "MongoDB"]
